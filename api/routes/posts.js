7026bf983932f039dc0919f6940149bf5fccddef
AikshitGarg <aikshitgarg@gmail.com>
2024-03-04 01:41:19 +0530

    code saved

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -54,6 +54,7 @@ router.delete("/:id", async (req, res) => {
         }
     }
     catch (err){
+        console.log(err);
         res.status(500).json(err);
     }
 });
--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,21 +1,43 @@
 import { Link, useLocation } from "react-router-dom"
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./singlePost.css"
-import axios from 'axios';
+import axios from "axios";
+import { Context } from "../../context/Context";
 
 export default function SinglePost() {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
   const [post, setPost] = useState({});
-  const PF = "http://localhost:5000/images/"
-
+  const PF = "http://localhost:5000/images/";
+  const { user } = useContext(Context);
+  const [title, setTitle] = useState();
+  const [desc, setDesc] = useState("");
+  const [updateMode, setUpdateMode] = useState(false);
+  
   useEffect(() => {
     const getPost = async() => {
       const res = await axios.get("/posts/" + path);
       setPost(res.data);
+      setTitle(res.data.title);
+      setDesc(res.data.desc);
     };
     getPost();
   }, [path]);
+
+  const handleDelete = async () => {  
+    try{
+      await axios.delete(`/posts/${post._id}`, {data : {username : user.username},
+    });
+      window.location.replace("/");
+    }catch(err){}
+  };
+
+  const handleUpdate = async () => {
+    try{
+      await axios.put(`/posts/${post._id}`, {username : user.username, title, desc});
+      window.location.reload();
+    }catch(err){}
+  }
 
   return (
     <div className = "singlePost">
@@ -25,15 +47,19 @@ export default function SinglePost() {
             alt = "" 
             className = "singlePostImg"
           />
-        )}
-        <h1 className = "singlePostTitle">
-          {post.title}
-          <div className="singlePostEdit">
-            <i className ="singlePostIcon fa-regular fa-pen-to-square"></i>
-            <i className="singlePostIcon fa-solid fa-trash-can"></i>
-          </div>
-        </h1>
-
+        )}{
+          updateMode ? <input type = "text" className = "singlePostTitleInput" value = {title}  onChange = {(e) => setTitle(e.target.value)}/> : (
+            <h1 className = "singlePostTitle">
+              {post.title}
+              {post.username === user?.username && (
+              <div className="singlePostEdit">
+                <i className ="singlePostIcon fa-regular fa-pen-to-square" onClick={() => setUpdateMode(true)}></i>
+                <i className="singlePostIcon fa-solid fa-trash-can" onClick = {handleDelete}></i>
+              </div>
+              )}
+            </h1>                
+          )
+        }
         <div className = "singlePostInfo">
           <span className = "singlePostAuthor"> Author : 
             <Link to = {`/?user=${post.username}`} className = "link">
@@ -42,9 +68,12 @@ export default function SinglePost() {
           </span>
           <span className = "singlePostDate"> {new Date(post.createdAt).toDateString()} </span>
         </div>
-        <p className = "singlePostDesc">
-          {post.desc}
-        </p>
+        {updateMode ? (<textarea className = "singlePostDescInput" value = {desc} onChange = {(e) => setDesc(e.target.value)} />) :( 
+          <p className = "singlePostDesc">
+            {post.desc}
+          </p>
+        )}
+        <button className = "singlePostButton" onClick = {handleUpdate} >Update</button>     
       </div>
     </div>
   )
