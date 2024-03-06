d4a6ba98061e4c1564795e31bb5a0168d3e07fe3
AikshitGarg <aikshitgarg@gmail.com>
2024-03-07 00:42:22 +0530

    title updated

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
   return (
     <div className = "header">
         <div className = "headerTitles">
-            <span className = "headerTitleSm"> React & Node </span>
-            <span className = "headerTitleLg"> Blog </span>
+            <span className = "headerTitleSm"> Echo </span>
+            <span className = "headerTitleLg"> Express </span>
         </div>
         <img className = "headerImg" src = "https://www.jessleephotos.com/images/xl/typsp1-1740.jpg" alt = ""/>
     </div>
