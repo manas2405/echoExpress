5dfeb992ca3950dd9d745fe38577360cd6fd66c8
AikshitGarg <aikshitgarg@gmail.com>
2024-03-06 14:13:50 +0530

    code pushed

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,16 +1,13 @@
-import "./posts.css";
-import Post from "../post/Post";
+import "./posts.css"
+import Post from "../post/Post"
 
-export default function Posts({ posts }) {
+export default function Posts({posts}) {
   return (
-    <div className="posts">
-      {Array.isArray(posts) && posts.length > 0 ? (
-        posts.map((post) => (
-          <Post key={post._id} post={post} />
-        ))
-      ) : (
-        <p>No posts available.</p>
-      )}
+    <div className = "posts">
+      {posts?.map((post) => (
+        <Post key = {post._id} post = {post} />
+      ))}
     </div>
-  );
+  )
 }
+
