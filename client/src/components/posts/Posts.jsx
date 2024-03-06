3344fb1164574cba60d93ca93493fa28c6a6c9bf
AikshitGarg <aikshitgarg@gmail.com>
2024-03-06 14:04:26 +0530

    code updated

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -4,12 +4,12 @@ import Post from "../post/Post";
 export default function Posts({ posts }) {
   return (
     <div className="posts">
-      {posts ? (
+      {Array.isArray(posts) && posts.length > 0 ? (
         posts.map((post) => (
           <Post key={post._id} post={post} />
         ))
       ) : (
-        <p>No posts to display.</p>
+        <p>No posts available.</p>
       )}
     </div>
   );
