0aeb410134adc49c444570dbc8ab21225be7a8c7
aikshitgarg <91785194+aikshit123@users.noreply.github.com>
2024-03-15 14:09:06 +0530

    Update apiPaths.js

--- client/src/apiPaths.js
+++ client/src/apiPaths.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "https://echoexpress.onrender.com/api";
-const IMAGE_URL = "https://echoexpress.onrender.com/images/"
+const API_BASE_URL = "https://echoexpress-1.onrender.com/api";
+const IMAGE_URL = "https://echoexpress-1.onrender.com/images/"
 
 export const BACKEND_URL = `${API_BASE_URL}`;
 export const PF =  `${IMAGE_URL}`;
