85f405eaa244102cdff7dbe57dc28978632060b1
Manas Mittal <mrmanasmittal@gmail.com>
2024-05-01 14:52:05 +0530

    frontend and backend added

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
@@ -12,7 +13,6 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 
-dotenv.config();
 
 app.use(cors({
   origin: '*',
@@ -24,7 +24,10 @@ app.use(cors({
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname,"/images")))
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser:true ,
+    useUnifiedTopology: true ,
+})
 .then(console.log("Connected to MongoDb"))
 .catch((err) => console.log(err));
 
@@ -48,7 +51,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-console.log("hello");
 
 app.use("/lama", (req, res) => {
     console.log("hey this is lama url");
